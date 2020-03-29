9a978c8599f716f9ba15ddf0608689574a5ae6e3
gennadiy123 <shelukhin_g@ukr.net>
2020-03-29 20:21:35 +0300

    homework

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-import HomePage from "./pages/HomePage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
+import React, { lazy, Suspense } from "react";
+// import HomePage from "./pages/HomePage";
+// import MovieDetailsPage from "./pages/MovieDetailsPage";
+// import MoviesPage from './pages/MoviesPage';
+
 import { NavLink, Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 
 const activeStyle = {
   color: "palevioletred"
 };
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage'));
 
 const App = () => (
   <>
@@ -18,9 +24,11 @@ const App = () => (
       Movies
     </NavLink>
     <Switch>
+      <Suspense fallback={<div>Loading...</div>}>
       <Route exact path="/" component={HomePage} />
       <Route path='/movies/:movieId' component={MovieDetailsPage} />
-      <Route exact path="/movies" component={MovieDetailsPage} />
+      <Route exact path="/movies" component={MoviesPage} />
+      </Suspense>
       <Redirect to="/" />
     </Switch>
   </>
